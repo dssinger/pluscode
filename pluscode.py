25d0c2fd227aa4af695b6ffe4690add49b0b8352
David Singer <david@dsinger.com>
2020-09-02 21:43:45 -0700

    Handles any address Google Maps will use

--- pluscode.py
+++ pluscode.py
@@ -11,12 +11,13 @@ if '°' in coord and coord[0] in 'NS':
     # put the hemisphere at the end of the coordinates to make Lightroom happy
     # N 40° 53.982 W 072° 52.167 ==> 40° 53.982 N 072° 52.167 W
     print(' '.join([sys.argv[i] for i in (2,3,1,5,6,4)]))
-elif '+' in coord:
-    gmaps = googlemaps.Client(key=key)
-    result = gmaps.geocode(' '.join(sys.argv[1:]))[0]['geometry']['location']
-    print(f"{result['lat']},{result['lng']}")
 else:
-    sys.stderr.write(f"Can't convert {coord}\n")
-    print(sys.argv[1:])
+    gmaps = googlemaps.Client(key=key)
+    try:
+        result = gmaps.geocode(' '.join(sys.argv[1:]))[0]['geometry']['location']
+        print(f"{result['lat']},{result['lng']}")
+    except (KeyError, IndexError):
+        sys.stderr.write(f"Can't convert {coord}\n")
+        print(sys.argv[1:])
 
 
